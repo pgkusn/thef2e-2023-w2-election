3f801a45631da0c9cd81d84f790e6199e8e12d20
Kenge <pgkusn@gmail.com>
2024-06-19 23:31:03 +0800

    重構型別定義

--- types.ts
+++ types.ts
@@ -6,6 +6,10 @@ export interface ApiCity {
   CountyID: string
   Version: string
 }
+interface ApiCityCodeAndName {
+  city_code: string // 縣市代碼
+  city_name: string // 縣市名稱
+}
 export interface ApiAllVote {
   candidate_1: string // 1號候選人得票數
   candidate_2: string // 2號候選人得票數
@@ -15,37 +19,12 @@ export interface ApiAllVote {
   votes: string // 投票數
   rate: string // 投票率
 }
-export interface ApiAllCityVote {
-  city_code: string // 縣市代碼
-  city_name: string // 縣市名稱
-  candidate_1: string // 1號候選人得票數
-  candidate_2: string // 2號候選人得票數
-  candidate_3: string // 3號候選人得票數
-  valid_votes: string // 有效票數
-  invalid_votes: string // 無效票數
-  votes: string // 投票數
-  rate: string // 投票率
+interface ApiCityVoteDist extends ApiAllVote {
+  dist_name: string // 行政區名稱
 }
-export interface ApiCityVote {
-  city_code: string // 縣市代碼
-  city_name: string // 縣市名稱
-  candidate_1: string // 1號候選人得票數
-  candidate_2: string // 2號候選人得票數
-  candidate_3: string // 3號候選人得票數
-  valid_votes: string // 有效票數
-  invalid_votes: string // 無效票數
-  votes: string // 投票數
-  rate: string // 投票率
-  dist: {
-    dist_name: string // 行政區名稱
-    candidate_1: string // 1號候選人得票數
-    candidate_2: string // 2號候選人得票數
-    candidate_3: string // 3號候選人得票數
-    valid_votes: string // 有效票數
-    invalid_votes: string // 無效票數
-    votes: string // 投票數
-    rate: string // 投票率
-  }[]
+export interface ApiAllCityVote extends ApiCityCodeAndName, ApiAllVote {}
+export interface ApiCityVote extends ApiAllCityVote {
+  dist: ApiCityVoteDist[]
 }
 export interface ApiFetchError {
   statusCode: number
@@ -62,42 +41,19 @@ export interface TotalVotes {
   rate: number // 投票率
   candidate: number[] // 候選人得票數
 }
-export interface AllCityVotes {
+interface CityCodeAndName {
   cityCode: string // 縣市代碼
   cityName: string // 縣市名稱
-  validVotes: number // 有效票數
-  invalidVotes: number // 無效票數
-  votes: number // 投票數
-  rate: number // 投票率
-  candidate: number[] // 候選人得票數
 }
-export interface CityVotes {
-  cityCode: string // 縣市代碼
-  cityName: string // 縣市名稱
-  validVotes: number // 有效票數
-  invalidVotes: number // 無效票數
-  votes: number // 投票數
-  rate: number // 投票率
-  candidate: number[] // 候選人得票數
-  dist: {
-    distName: string // 行政區名稱
-    validVotes: number // 有效票數
-    invalidVotes: number // 無效票數
-    votes: number // 投票數
-    rate: number // 投票率
-    candidate: number[] // 候選人得票數
-  }[]
+export interface AllCityVotes extends CityCodeAndName, TotalVotes {}
+interface CityVotesDist extends AllCityVotes {
+  distName: string // 行政區名稱
+}
+export interface CityVotes extends AllCityVotes {
+  dist: CityVotesDist[]
 }
 export interface CityGroup {
-  [key: string]: {
-    cityCode: string
-    cityName: string
-    validVotes: number
-    invalidVotes: number
-    votes: number
-    rate: number
-    candidate: number[]
-  }
+  [key: string]: AllCityVotes
 }
 export interface MapData {
   [key: string]: {
